984b3f2e1c605c236c880c4ffe2d1493dd3b32c6
paulo <paulo-3425@hotmail.com>
2020-01-22 14:18:56 -0300

    Initial commit

---
+++ src/test/java/com/fileimport/FileimportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileimportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
